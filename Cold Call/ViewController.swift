e916c43da7d92718e8acac5321af208ba45d8a5e
Amal001001 <amalalmutairi001@gmail.com>
2021-11-29 17:30:56 +0000

    cold call

--- Cold Call/ViewController.swift
+++ Cold Call/ViewController.swift
@@ -6,7 +6,6 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var namesLabel: UILabel!
-    @IBOutlet weak var numberLable: UILabel!
     
     let names = ["amal","bob","jill","jack","leon","ada","salah"]
     
@@ -21,11 +20,6 @@ class ViewController: UIViewController {
         let currentName = Int.random(in: 0..<names.count)
         namesLabel.text = names[currentName]
         
-        let currentNumber = Int.random(in: 1...5)
-        if currentNumber == 1 || currentNumber == 2{numberLable.textColor = UIColor.red}
-        else if currentNumber == 3 || currentNumber == 4{numberLable.textColor = UIColor.orange}
-        else {numberLable.textColor = UIColor.green}
-        numberLable.text = "\(currentNumber)"
     }
 
 }
